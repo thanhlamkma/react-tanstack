1f4d0645b67eb48234dcad377af865528e0a4346
Grela <vtlam134@gmail.com>
2025-04-20 10:05:35 +0700

    init

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,61 +1,68 @@
-import { createRootRoute, Link, Outlet, useRouterState } from "@tanstack/react-router";
+import {
+  createRootRoute,
+  Link,
+  Outlet,
+  useRouterState,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Spinner } from "../components/Spinner";
 
 function RouterSpinner() {
-  const isLoading = useRouterState({ select: (s) => s.status === 'pending'});
+  const isLoading = useRouterState({ select: (s) => s.status === "pending" });
   return <Spinner show={isLoading} />;
 }
 
 export const Route = createRootRoute({
   component: () => {
-    return <>
-<div className={`min-h-screen flex flex-col`}>
-        <div className={`flex items-center border-b gap-2`}>
-          <h1 className={`text-3xl p-2`}>Grela</h1>
-          {/* Show a global spinner when the router is transitioning */}
-          <div className={`text-3xl`}>
-            <RouterSpinner />
+    return (
+      <>
+        <div className={`min-h-screen flex flex-col`}>
+          <div className={`flex items-center border-b gap-2`}>
+            <h1 className={`text-3xl p-2`}>Grela</h1>
+            {/* Show a global spinner when the router is transitioning */}
+            <div className={`text-3xl`}>
+              <RouterSpinner />
+            </div>
           </div>
-        </div>
-        <div className={`flex-1 flex`}>
-          <div className={`divide-y w-56`}>
-            {(
-              [
-                ['/', 'Home'],
-                ['/about', 'About'],
-                ['/pomodoro', 'Pomodoro'],
-              ] as const
-            ).map(([to, label]) => {
-              return (
-                <div key={to}>
-                  <Link
-                    to={to}
-                    activeOptions={
-                      {
-                        // If the route points to the root of it's parent,
-                        // make sure it's only active if it's exact
-                        // exact: to === '.',
+          <div className={`flex-1 flex`}>
+            <div className={`divide-y w-56`}>
+              {(
+                [
+                  ["/", "Home"],
+                  ["/about", "About"],
+                  ["/pomodoro", "Pomodoro"],
+                ] as const
+              ).map(([to, label]) => {
+                return (
+                  <div key={to}>
+                    <Link
+                      to={to}
+                      activeOptions={
+                        {
+                          // If the route points to the root of it's parent,
+                          // make sure it's only active if it's exact
+                          // exact: to === '.',
+                        }
                       }
-                    }
-                    preload="intent"
-                    className={`block py-2 px-3 text-blue-700`}
-                    // Make "active" links bold
-                    activeProps={{ className: `font-bold` }}
-                  >
-                    {label}
-                  </Link>
-                </div>
-              )
-            })}
-          </div>
-          <div className={`flex-1 border-l`}>
-            <Outlet />
+                      preload="intent"
+                      className={`block py-2 px-3 text-blue-700`}
+                      // Make "active" links bold
+                      activeProps={{ className: `font-bold` }}
+                    >
+                      {label}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+            <div className={`flex-1 border-l`}>
+              <Outlet />
+            </div>
           </div>
         </div>
-      </div>
-      <TanStackRouterDevtools position="bottom-right" />
-    </>
+        <TanStackRouterDevtools position="bottom-right" />
+      </>
+    );
   },
   notFoundComponent: () => {
     return (
@@ -63,6 +70,6 @@ export const Route = createRootRoute({
         <p>This is the notFoundComponent configured on root route</p>
         <Link to="/">Start Over</Link>
       </div>
-    )
+    );
   },
-})
+});
